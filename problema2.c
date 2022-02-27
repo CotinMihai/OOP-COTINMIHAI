08cb39cd208657efad63075246473286e14666ed
CotinMihai <cotinmihai@gmail.com>
2022-02-27 16:37:23 +0200

    Update problema2.c

--- problema2.c
+++ problema2.c
@@ -10,38 +10,41 @@ int main()
 {
 printf("Introduceti numarul de cuvinte din propozitie: ");
 	int numarCuvinte;
-	scanf("%d", &numarCuvinte, 4);
-	printf("Introduceti o propozitie: ");
+	scanf("%d", &numarCuvinte, 200);
+	//printf("Introduceti o propozitie: ");
 	char cuvant[100];
 	//scanf("%19s", cuvant);
 	int counter = 0;
-	while (scanf("%19s", cuvant, 20))
+	do
 	{
 		//if (strcmp(cuvant, "\n\n") == 0)
 		//	break;
-		printf("Cuvantul citit este: %s\n", cuvant);
+		printf("Dati cuvantul:\n");
+		scanf("%19s", cuvant, 20);
 		counter += 1;
 		strcpy(m[counter],cuvant);
-		if (counter >= numarCuvinte)
-			break;
-	}
+	
+		
+	}while (counter < numarCuvinte);
 
-for(i=1;i<counter;i++)
-{for(j=i+1;j<=counter;j++)
-printf("%d %d",l1,l2);
-	l1=strlen(m[i]);
-	l2=strlen(m[j]);
-	if((l1<l2)||(l1==l2&&strcmp(m[i],m[j])>0))
-{
-	strcpy(aux,m[i]);
-	strcpy(m[i],m[j]);
-	strcpy(m[j],aux);
-}
-}
-
+	for (i=1;i<n;i++)
+	for (j=i+1;j<=n;j++)
+	  {
+		  if (strlen(m[i])<strlen(m[j]))
+	     {
+			 strcpy(aux,m[i]);
+			 strcpy(m[i],m[j]);
+			 strcpy(m[j],aux);
+		 }
+		 else if (strlen(m[i])==strlen(m[j]) && strcmp(m[i],m[j])>0) 
+		 {
+			 strcpy(aux,m[i]);
+			 strcpy(m[i],m[j]);
+			 strcpy(m[j],aux);
+		}
+	  }
 for(i=1;i<=counter;i++)
-printf("%s\n", m[i]);
-
+    printf("%s\n",m[i]);
  return 0;
  }
 
